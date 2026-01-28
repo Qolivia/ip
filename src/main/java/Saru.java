d0315c4b0f5398853ea9496ee7725251d9420e7f
Eunwoo <olivianoony62@gmail.com>
2026-01-28 22:46:15 +0800

    Level 2. Add, List

--- src/main/java/Saru.java
+++ src/main/java/Saru.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class Saru {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Hello! I'm Saru");
+        String[] tasks = new String[100];
+        int cnt = 0;
+;        System.out.println("Hello! I'm Saru");
         System.out.println("What can I do for you?");
 
         while (true) {
@@ -13,8 +15,16 @@ public class Saru {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
+            if (input.equals("list")) {
+                for (int i = 0; i < cnt; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                continue;
+            }
+            tasks[cnt] = input;
+            cnt++;
 
-            System.out.println(input);
+            System.out.println("added: " + input);
         }
     }
 }
