dac72edef90affd7727f08c65be44ce3d06e2a4d
Eunwoo <olivianoony62@gmail.com>
2026-01-28 17:51:54 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Saru.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Saru {
+    public static void main(String[] args) {
+        //Scanner sc = new Scanner(System.in);
+        System.out.println("Hello! I'm Saru");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+        /*while (true) {
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+
+            System.out.println(input);
+        }*/
+    }
+}
